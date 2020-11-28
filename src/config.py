1c64c60a2d46381e19154a23053b3c3a5862f440
Aditya Pal <aditya.pal.science@gmail.com>
2020-11-28 19:12:21 +0530

    Removed BERT Path

--- src/config.py
+++ src/config.py
@@ -5,7 +5,6 @@ MAX_LEN = 64
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 10
-# BERT_PATH = "input/bert-base-uncased/"
 MODEL_PATH = "model.bin"
 TRAINING_FILE = "input/imdb.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
